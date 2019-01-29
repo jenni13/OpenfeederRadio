a21e5f27e6ad45b997507e5282ee1b670f096efd
jenni <jenni@172.17.54.89>
2019-01-29 13:56:49 +0100

    test

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jenni
+ *
+ * Created on January 29, 2019, 1:56 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
